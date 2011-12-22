64774c30a8f8705c4d77e991f6f8235e3bacd0ae
Georg Tavonius <georg.tavonius@xing.com>
2011-12-23 00:29:05 +0100

    connecting to mysql database works

--- test/connection_test.js
+++ test/connection_test.js
@@ -1,7 +1,12 @@
 var Seq = require(__dirname + '/..'),
+    mysql = require('mysql'),
     TEST_CONFIG = require(__dirname + '/test_config');
+    client = mysql.createClient(TEST_CONFIG);
 
 module.exports = {
+  tearDown: function(cb) {
+    client.query("DROP TABLE test;", function() { cb(); });
+  },
   'test creating a connection object without parameters throws': function(test) {
     test.throws(function() {
       Seq.create();
@@ -12,5 +17,29 @@ module.exports = {
     var instance = Seq.create(TEST_CONFIG);
     test.equal(typeof instance, 'object', 'we created an object');
     test.done();
+  },
+  'test creating connector does not open the connection immediately': function(test) {
+    var instance = Seq.create(TEST_CONFIG);
+    test.ok(!instance.isConnected, 'should not connect until first sql goes out');
+    test.done();
+  },
+  'test executing sql works': function(test) {
+    var instance = Seq.create(TEST_CONFIG);
+    instance.query('CREATE TABLE test (`foo` TEXT NOT NULL);', function(err, info) {
+      if (err) throw err;
+      client.query('DESCRIBE test;', function(err, result) {
+        if (err) throw err;
+        test.equal(result.length, 1, 'We should have a table with only one field');
+        test.equal(result[0].Field, 'foo')
+        test.done();
+      })
+    });
+  },
+  'test first execution of sql starts a connection': function(test) {
+    var instance = Seq.create(TEST_CONFIG);
+    instance.query('SELECT id FROM dummy;', function(err, info) {
+      test.ok(instance.isConnected, 'we should be connected by now');
+      test.done();
+    });
   }
 };
