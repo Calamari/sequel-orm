c60da89b8913cdcfcdedb76277e2c5eaaff238e5
Georg Tavonius <georg.tavonius@xing.com>
2011-12-23 14:44:59 +0100

    Creating tables with columns part I

--- test/migration_test.js
+++ test/migration_test.js
@@ -1,6 +1,6 @@
 var Seq = require(__dirname + '/..'),
     mysql = require('mysql'),
-    TEST_CONFIG = require(__dirname + '/test_config');
+    TEST_CONFIG = require(__dirname + '/test_config'),
     client = mysql.createClient(TEST_CONFIG);
 
 module.exports = {
@@ -31,6 +31,31 @@ module.exports = {
         test.equal(result[0].Key, 'PRI');
         test.equal(result[0].Extra, 'auto_increment');
         test.equal(result[0].Type, 'int(11)');
+        test.done();
+      });
+    });
+  },
+  'test adding column without valid data type returns error': function(test) {
+    this.db.createTable('products', function(table) {
+      table.addColumn('foo', 'VARCHAR(11)');
+    }, function(err) {
+      test.equal(err.constructor, Seq.errors.NotADataTypeError, "Error should be of Type NotADataTypeError");
+      test.done();
+    });
+  },
+  'test adding columns to a table': function(test) {
+    this.db.createTable('products', function(table) {
+      table.addColumn('foo', Seq.dataTypes.VARCHAR);
+      table.addColumn('my_time', Seq.dataTypes.DATETIME);
+      table.addColumn('myint', Seq.dataTypes.INT);
+      table.addColumn('floating', Seq.dataTypes.FLOAT);
+      table.addColumn('sure', Seq.dataTypes.BOOLEAN);
+      table.addColumn('text', Seq.dataTypes.TEXT);
+    }, function(err) {
+      if (err) throw err;
+      client.query("DESCRIBE products;", function(err, result) {
+        if (err) throw err;
+        test.equal(result.length, 7, 'We should have a table with all seven fields');
         test.done();
       });
     });
