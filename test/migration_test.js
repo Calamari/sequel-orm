d30917488f108c8cea3f9250be9004c0bf9ac112
Georg Tavonius <georg.tavonius@xing.com>
2011-12-23 19:23:02 +0100

    default values in columns

--- test/migration_test.js
+++ test/migration_test.js
@@ -126,14 +126,49 @@ module.exports = {
       client.query("DESCRIBE products;", function(err, result) {
         if (err) throw err;
         test.equal(result.length, 9, 'We should have a table with nine field');
-        test.equal(result[1].Null, 'YES', result[1].Field + 'should be nullable');
-        test.equal(result[2].Null, 'YES', result[2].Field + 'should be nullable');
-        test.equal(result[3].Null, 'YES', result[3].Field + 'should be nullable');
-        test.equal(result[4].Null, 'YES', result[4].Field + 'should be nullable');
-        test.equal(result[5].Null, 'NO', result[5].Field + 'should be not nullable');
-        test.equal(result[6].Null, 'NO', result[6].Field + 'should be not nullable');
-        test.equal(result[7].Null, 'NO', result[7].Field + 'should be not nullable');
-        test.equal(result[8].Null, 'NO', result[8].Field + 'should be not nullable');
+        test.equal(result[1].Null, 'YES', result[1].Field + ' should be nullable');
+        test.equal(result[2].Null, 'YES', result[2].Field + ' should be nullable');
+        test.equal(result[3].Null, 'YES', result[3].Field + ' should be nullable');
+        test.equal(result[4].Null, 'YES', result[4].Field + ' should be nullable');
+        test.equal(result[5].Null, 'NO', result[5].Field + ' should be not nullable');
+        test.equal(result[6].Null, 'NO', result[6].Field + ' should be not nullable');
+        test.equal(result[7].Null, 'NO', result[7].Field + ' should be not nullable');
+        test.equal(result[8].Null, 'NO', result[8].Field + ' should be not nullable');
+        test.done();
+      });
+    });
+  },
+  'test datetime columns can not have a default value': function(test) {
+    this.db.createTable('products', function(table) {
+      table.addColumn('def_datetime', Seq.dataTypes.DATETIME({ default: 'NOW' }));
+    }, function(err) {
+      test.equal(err.constructor, Seq.errors.DefaultValueForbiddenError, "Error should be of Type DefaultValueForbiddenError");
+      test.done();
+    });
+  },
+  'test making columns having default values': function(test) {
+    this.db.createTable('products', function(table) {
+      table.addColumn('def', Seq.dataTypes.VARCHAR({ default: 'some text' }));
+      table.addColumn('def_int', Seq.dataTypes.INT({ default: 42 }));
+      table.addColumn('def_float', Seq.dataTypes.FLOAT({ default: 42.5 }));
+      table.addColumn('def_bool', Seq.dataTypes.BOOLEAN({ default: true }));
+      table.addColumn('no_def', Seq.dataTypes.VARCHAR({ default: false }));
+      table.addColumn('no_def_int', Seq.dataTypes.INT({ default: false }));
+      table.addColumn('no_def_float', Seq.dataTypes.FLOAT({ default: false }));
+      table.addColumn('no_def_bool', Seq.dataTypes.BOOLEAN({ default: false }));
+    }, function(err) {
+      if (err) throw err;
+      client.query("DESCRIBE products;", function(err, result) {
+        if (err) throw err;
+        test.equal(result.length, 9, 'We should have a table with nine field');
+        test.equal(result[1].Default, 'some text', result[1].Field + ' should have default value');
+        test.equal(result[2].Default, 42, result[2].Field + ' should have default value');
+        test.equal(result[3].Default, 42.5, result[3].Field + ' should have default value');
+        test.equal(result[4].Default, 1, result[4].Field + ' should have default value');
+        test.equal(result[5].Default, null, result[5].Field + ' should not have default value');
+        test.equal(result[6].Default, null, result[6].Field + ' should not have default value');
+        test.equal(result[7].Default, null, result[7].Field + ' should not have default value');
+        test.equal(result[8].Default, null, result[8].Field + ' should not have default value');
         test.done();
       });
     });
