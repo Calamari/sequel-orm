8b321d63f5ea51825b09956189e8ae00706d54ec
Georg Tavonius <georg.tavonius@xing.com>
2011-12-23 18:42:01 +0100

    varchar length and NOT NULL

--- test/data_types_test.js
+++ test/data_types_test.js
@@ -2,97 +2,97 @@ var types = require(__dirname + '/..').dataTypes;
 
 module.exports = {
   'test INT specification': function(test) {
-    test.equal(types.INT.type, 'integer');
-    test.equal(types.INT.sql, 'INT(#{length})');
-    test.equal(types.INT.defaultOptions.length, 11);
+    test.equal(types.INT().type, 'integer');
+    test.equal(types.INT().sql, 'INT(#{length})');
+    test.equal(types.INT().defaultOptions.length, 11);
     test.done();
   },
   'test INT validation': function(test) {
-    test.ok(types.INT.validation(3));
-    test.ok(types.INT.validation(0));
-    test.ok(!types.INT.validation("3"));
-    test.ok(!types.INT.validation(42.6));
-    test.ok(!types.INT.validation("54.6"));
-    test.ok(!types.INT.validation("D"));
-    test.ok(!types.INT.validation(Number.NaN));
+    test.ok(types.INT().validation(3));
+    test.ok(types.INT().validation(0));
+    test.ok(!types.INT().validation("3"));
+    test.ok(!types.INT().validation(42.6));
+    test.ok(!types.INT().validation("54.6"));
+    test.ok(!types.INT().validation("D"));
+    test.ok(!types.INT().validation(Number.NaN));
     test.done();
   },
   'test VARCHAR specification': function(test) {
-    test.equal(types.VARCHAR.type, 'varchar');
-    test.equal(types.VARCHAR.sql, 'VARCHAR(#{length})');
-    test.equal(types.VARCHAR.defaultOptions.length, 255);
+    test.equal(types.VARCHAR().type, 'varchar');
+    test.equal(types.VARCHAR().sql, 'VARCHAR(#{length})');
+    test.equal(types.VARCHAR().defaultOptions.length, 255);
     test.done();
   },
   'test VARCHAR validation': function(test) {
-    test.ok(types.VARCHAR.validation("Hallo"));
-    test.ok(types.VARCHAR.validation(""));
-    test.ok(!types.VARCHAR.validation(4));
-    test.ok(!types.VARCHAR.validation(42.6));
-    test.ok(!types.VARCHAR.validation(null));
-    test.ok(!types.VARCHAR.validation(function() {}));
+    test.ok(types.VARCHAR().validation("Hallo"));
+    test.ok(types.VARCHAR().validation(""));
+    test.ok(!types.VARCHAR().validation(4));
+    test.ok(!types.VARCHAR().validation(42.6));
+    test.ok(!types.VARCHAR().validation(null));
+    test.ok(!types.VARCHAR().validation(function() {}));
     test.done();
   },
   'test TEXT specification': function(test) {
-    test.equal(types.TEXT.type, 'text');
-    test.equal(types.TEXT.sql, 'TEXT');
-    test.equal(types.TEXT.defaultOptions, null);
+    test.equal(types.TEXT().type, 'text');
+    test.equal(types.TEXT().sql, 'TEXT');
+    test.equal(types.TEXT().defaultOptions, null);
     test.done();
   },
   'test TEXT validation': function(test) {
-    test.ok(types.TEXT.validation("Hallo"));
-    test.ok(types.TEXT.validation(""));
-    test.ok(!types.TEXT.validation(4));
-    test.ok(!types.TEXT.validation(42.6));
-    test.ok(!types.TEXT.validation(null));
-    test.ok(!types.TEXT.validation(function() {}));
+    test.ok(types.TEXT().validation("Hallo"));
+    test.ok(types.TEXT().validation(""));
+    test.ok(!types.TEXT().validation(4));
+    test.ok(!types.TEXT().validation(42.6));
+    test.ok(!types.TEXT().validation(null));
+    test.ok(!types.TEXT().validation(function() {}));
     test.done();
   },
   'test BOOLEAN specification': function(test) {
-    test.equal(types.BOOLEAN.type, 'boolean');
-    test.equal(types.BOOLEAN.sql, 'INT(1)');
-    test.equal(types.BOOLEAN.defaultOptions, null);
+    test.equal(types.BOOLEAN().type, 'boolean');
+    test.equal(types.BOOLEAN().sql, 'INT(1)');
+    test.equal(types.BOOLEAN().defaultOptions, null);
     test.done();
   },
   'test BOOLEAN validation': function(test) {
-    test.ok(types.BOOLEAN.validation(true));
-    test.ok(types.BOOLEAN.validation(false));
-    test.ok(!types.BOOLEAN.validation("Hallo"));
-    test.ok(!types.BOOLEAN.validation(""));
-    test.ok(!types.BOOLEAN.validation({}));
-    test.ok(!types.BOOLEAN.validation(3));
-    test.ok(!types.BOOLEAN.validation(64.3));
-    test.ok(!types.BOOLEAN.validation(function() {}));
+    test.ok(types.BOOLEAN().validation(true));
+    test.ok(types.BOOLEAN().validation(false));
+    test.ok(!types.BOOLEAN().validation("Hallo"));
+    test.ok(!types.BOOLEAN().validation(""));
+    test.ok(!types.BOOLEAN().validation({}));
+    test.ok(!types.BOOLEAN().validation(3));
+    test.ok(!types.BOOLEAN().validation(64.3));
+    test.ok(!types.BOOLEAN().validation(function() {}));
     test.done();
   },
   'test FLOAT specification': function(test) {
-    test.equal(types.FLOAT.type, 'float');
-    test.equal(types.FLOAT.sql, 'FLOAT');
-    test.equal(types.FLOAT.defaultOptions, null);
+    test.equal(types.FLOAT().type, 'float');
+    test.equal(types.FLOAT().sql, 'FLOAT');
+    test.equal(types.FLOAT().defaultOptions, null);
     test.done();
   },
   'test FLOAT validation': function(test) {
-    test.ok(types.FLOAT.validation(4));
-    test.ok(types.FLOAT.validation(42.3));
-    test.ok(!types.FLOAT.validation("42.3"));
-    test.ok(!types.FLOAT.validation("fd"));
-    test.ok(!types.FLOAT.validation(Number.NaN));
+    test.ok(types.FLOAT().validation(4));
+    test.ok(types.FLOAT().validation(42.3));
+    test.ok(!types.FLOAT().validation("42.3"));
+    test.ok(!types.FLOAT().validation("fd"));
+    test.ok(!types.FLOAT().validation(Number.NaN));
     test.done();
   },
   'test DATETIME specification': function(test) {
-    test.equal(types.DATETIME.type, 'datetime');
-    test.equal(types.DATETIME.sql, 'DATETIME');
-    test.equal(types.DATETIME.defaultOptions, null);
+    test.equal(types.DATETIME().type, 'datetime');
+    test.equal(types.DATETIME().sql, 'DATETIME');
+    test.equal(types.DATETIME().defaultOptions, null);
     test.done();
   },
   'test DATETIME validation': function(test) {
-    test.ok(types.DATETIME.validation(new Date()));
-    test.ok(types.DATETIME.validation(new Date(2011,11,11)));
-    test.ok(!types.DATETIME.validation((new Date(2011,11,11)).getTime()));
-    test.ok(!types.DATETIME.validation(242342342));
-    test.ok(!types.DATETIME.validation("today"));
-    test.ok(!types.DATETIME.validation(null));
-    test.ok(!types.DATETIME.validation(true));
-    test.ok(!types.DATETIME.validation({}));
+    test.ok(types.DATETIME().validation(new Date()));
+    test.ok(types.DATETIME().validation(new Date(2011,11,11)));
+    test.ok(!types.DATETIME().validation((new Date(2011,11,11)).getTime()));
+    test.ok(!types.DATETIME().validation(242342342));
+    test.ok(!types.DATETIME().validation("today"));
+    test.ok(!types.DATETIME().validation(null));
+    test.ok(!types.DATETIME().validation(true));
+    test.ok(!types.DATETIME().validation({}));
     test.done();
   }
 };
--- test/migration_test.js
+++ test/migration_test.js
@@ -45,17 +45,67 @@ module.exports = {
   },
   'test adding columns to a table': function(test) {
     this.db.createTable('products', function(table) {
-      table.addColumn('foo', Seq.dataTypes.VARCHAR);
-      table.addColumn('my_time', Seq.dataTypes.DATETIME);
-      table.addColumn('myint', Seq.dataTypes.INT);
-      table.addColumn('floating', Seq.dataTypes.FLOAT);
-      table.addColumn('sure', Seq.dataTypes.BOOLEAN);
-      table.addColumn('text', Seq.dataTypes.TEXT);
+      table.addColumn('foo', Seq.dataTypes.VARCHAR());
+      table.addColumn('my_time', Seq.dataTypes.DATETIME());
+      table.addColumn('myint', Seq.dataTypes.INT());
+      table.addColumn('floating', Seq.dataTypes.FLOAT());
+      table.addColumn('sure', Seq.dataTypes.BOOLEAN());
+      table.addColumn('text', Seq.dataTypes.TEXT());
     }, function(err) {
       if (err) throw err;
       client.query("DESCRIBE products;", function(err, result) {
         if (err) throw err;
         test.equal(result.length, 7, 'We should have a table with all seven fields');
+        test.done();
+      });
+    });
+  },
+  'test adding a VARCHAR column without arguments': function(test) {
+    this.db.createTable('products', function(table) {
+      table.addColumn('foo', Seq.dataTypes.VARCHAR());
+    }, function(err) {
+      if (err) throw err;
+      client.query("DESCRIBE products;", function(err, result) {
+        if (err) throw err;
+        test.equal(result.length, 2, 'We should have a table with only one field');
+        test.equal(result[1].Field, 'foo');
+        test.equal(result[1].Key, '');
+        test.equal(result[1].Extra, '');
+        test.equal(result[1].Type, 'varchar(255)');
+        test.done();
+      });
+    });
+  },
+  'test adding a VARCHAR column with overriding length': function(test) {
+    this.db.createTable('products', function(table) {
+      table.addColumn('foo', Seq.dataTypes.VARCHAR({ length: 32 }));
+    }, function(err) {
+      if (err) throw err;
+      client.query("DESCRIBE products;", function(err, result) {
+        if (err) throw err;
+        test.equal(result.length, 2, 'We should have a table with only one field');
+        test.equal(result[1].Field, 'foo');
+        test.equal(result[1].Null, 'NO');
+        test.equal(result[1].Key, '');
+        test.equal(result[1].Extra, '');
+        test.equal(result[1].Type, 'varchar(32)');
+        test.done();
+      });
+    });
+  },
+  'test making a VARCHAR nullable': function(test) {
+    this.db.createTable('products', function(table) {
+      table.addColumn('foo', Seq.dataTypes.VARCHAR({ allowNull: true }));
+    }, function(err) {
+      if (err) throw err;
+      client.query("DESCRIBE products;", function(err, result) {
+        if (err) throw err;
+        test.equal(result.length, 2, 'We should have a table with only one field');
+        test.equal(result[1].Field, 'foo');
+        test.equal(result[1].Null, 'YES');
+        test.equal(result[1].Key, '');
+        test.equal(result[1].Extra, '');
+        test.equal(result[1].Type, 'varchar(255)');
         test.done();
       });
     });
