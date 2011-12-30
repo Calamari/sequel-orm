6b52568a38bbc66d82572d572371b3eca1743ba1
Georg Tavonius <georg.tavonius@xing.com>
2011-12-30 11:38:01 +0100

    changed names in model_test

--- test/model_test.js
+++ test/model_test.js
@@ -17,56 +17,58 @@ module.exports.modelDefinition = {
     });
   },
   'test creating a model using data types': function(test) {
-    var Product = Seq.defineModel('Item', {
+    var Item = Seq.defineModel('Item', {
       name: Seq.dataTypes.VARCHAR(),
       price: Seq.dataTypes.INT(),
       createdAt: Seq.dataTypes.DATETIME(),
       updatedAt: Seq.dataTypes.DATETIME()
     });
-    test.equal(typeof Product, 'object');
-    test.equal(Product.name, 'Item');
-    test.equal(Product.tableName, 'items');
+    test.equal(typeof Item, 'object');
+    test.equal(Item.name, 'Item');
+    test.equal(Item.tableName, 'items');
     test.done();
   },
   'test getting tableName': function(test) {
-    var Product = Seq.defineModel('Miss', {
+    var Miss = Seq.defineModel('Miss', {
     });
-    test.equal(Product.tableName, 'miss', 'don\'t append a plural s at the end, if there is alredy one');
+    test.equal(Miss.tableName, 'miss', 'don\'t append a plural s at the end, if there is alredy one');
     test.done();
   },
   'test creating a model has fields': function(test) {
-    var Product = Seq.defineModel('Item', {
+    var Item = Seq.defineModel('Item', {
       name: Seq.dataTypes.VARCHAR(),
       price: Seq.dataTypes.INT(),
       createdAt: Seq.dataTypes.DATETIME(),
       updatedAt: Seq.dataTypes.DATETIME()
     });
-    test.equal(Product.fields.length, 5);
-    test.ok(Product.fields.indexOf('id') !== -1);
-    test.ok(Product.fields.indexOf('name') !== -1);
-    test.ok(Product.fields.indexOf('price') !== -1);
-    test.ok(Product.fields.indexOf('createdAt') !== -1);
-    test.ok(Product.fields.indexOf('updatedAt') !== -1);
+    test.equal(Item.fields.length, 5);
+    test.ok(Item.fields.indexOf('id') !== -1);
+    test.ok(Item.fields.indexOf('name') !== -1);
+    test.ok(Item.fields.indexOf('price') !== -1);
+    test.ok(Item.fields.indexOf('createdAt') !== -1);
+    test.ok(Item.fields.indexOf('updatedAt') !== -1);
     test.done();
   },
   'test create model from migration': function(test) {
-    var Product = Seq.defineModel('Item', Seq.getTableFromMigration('items'));
-    test.equal(Product.fields.length, 5);
-    test.ok(Product.fields.indexOf('id') !== -1);
-    test.ok(Product.fields.indexOf('name') !== -1);
-    test.ok(Product.fields.indexOf('price') !== -1);
-    test.ok(Product.fields.indexOf('createdAt') !== -1);
-    test.ok(Product.fields.indexOf('updatedAt') !== -1);
+    var Item = Seq.defineModel('Item', Seq.getTableFromMigration('items'));
+    test.equal(Item.fields.length, 5);
+    test.ok(Item.fields.indexOf('id') !== -1);
+    test.ok(Item.fields.indexOf('name') !== -1);
+    test.ok(Item.fields.indexOf('price') !== -1);
+    test.ok(Item.fields.indexOf('createdAt') !== -1);
+    test.ok(Item.fields.indexOf('updatedAt') !== -1);
     test.done();
   },
   'test retrieving of Model throuh SequelORM class': function(test) {
-    var Product = Seq.getModel('Item');
-    test.equal(Product.fields.length, 5);
-    test.ok(Product.fields[0], 'id');
-    test.ok(Product.fields[1], 'name');
-    test.ok(Product.fields[2], 'price');
-    test.ok(Product.fields[3], 'createdAt');
-    test.ok(Product.fields[4], 'updatedAt');
+    var Item = Seq.getModel('Item');
+    test.equal(Item.fields.length, 5);
+    test.ok(Item.fields[0], 'id');
+    test.ok(Item.fields[1], 'name');
+    test.ok(Item.fields[2], 'price');
+    test.ok(Item.fields[3], 'createdAt');
+    test.ok(Item.fields[4], 'updatedAt');
+    test.done();
+  },
     test.done();
   }
 };
