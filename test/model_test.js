4eedef7c9ab288fd80b193cc3d1e5d0d6fa3ed57
Georg Tavonius <georg.tavonius@xing.com>
2011-12-30 15:42:52 +0100

    started implementing model instance

--- test/model_test.js
+++ test/model_test.js
@@ -59,7 +59,7 @@ module.exports.modelDefinition = {
     test.ok(Item.fields.indexOf('updatedAt') !== -1);
     test.done();
   },
-  'test retrieving of Model throuh SequelORM class': function(test) {
+  'test retrieving of Model through SequelORM class': function(test) {
     var Item = Seq.getModel('Item');
     test.equal(Item.fields.length, 5);
     test.ok(Item.fields[0], 'id');
@@ -68,7 +68,61 @@ module.exports.modelDefinition = {
     test.ok(Item.fields[3], 'createdAt');
     test.ok(Item.fields[4], 'updatedAt');
     test.done();
+  }
+};
+
+module.exports.modelInstanciation = {
+  setUp: function(cb) {
+    var db  = Seq.create(TEST_CONFIG);
+    this.db = db;
+    client.query("DROP TABLE items;", function() {
+      Seq.createTable('items', function(table) {
+        table.addColumn('name', Seq.dataTypes.VARCHAR());
+        table.addColumn('price', Seq.dataTypes.INT());
+        table.addTimestamps();
+      });
+      cb();
+    });
   },
+  'test creating an instance with some given data': function(test) {
+    var Item = Seq.getModel('Item'),
+        item = Item.create({
+          name: 'John',
+          price: 42
+        });
+    test.equal(item.id, null);
+    test.equal(item.name, 'John');
+    test.equal(item.price, 42);
+    test.equal(item.createdAt, null);
+    test.equal(item.updatedAt, null);
+    test.done();
+  },
+  'test if element is marked as new and dirty': function(test) {
+    var Item = Seq.getModel('Item'),
+        item = Item.create({
+          name: 'John',
+          price: 42
+        });
+    test.equal(item.isNew, true);
+    test.equal(item.isDirty, true);
+    test.done();
+  },
+  'test adding instance methods': function(test) {
+    var Item = Seq.defineModel('Item', Seq.getTableFromMigration('items'), {
+      instanceMethods: {
+        testMe: function() {
+          return 42
+        },
+        foo: function(bar) {
+          return 'wuff ' + bar;
+        }
+      }
+    });
+    var item = Item.create();
+    test.equal(typeof item.testMe, 'function');
+    test.equal(typeof item.foo, 'function');
+    test.equal(item.testMe(), 42);
+    test.equal(item.foo('meauw'), 'wuff meauw');
     test.done();
   }
 };
