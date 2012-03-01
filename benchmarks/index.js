9fc5ca34f9cd4ebbe6d6306cb1e1ec365c14bcba
Georg Tavonius <georg.tavonius@xing.com>
2012-03-01 20:11:11 +0100

    added benchmark.js for testing

---
+++ benchmarks/index.js
@@ -0,0 +1,23 @@
+var Benchmark = require("Benchmark");
+
+
+var test = new Benchmark.Suite();
+
+test.add('RegExp#test', function() {
+  /o/.test('Hello World!');
+})
+.add('String#indexOf', function() {
+  'Hello World!'.indexOf('o') > -1;
+})
+.add('String#match', function() {
+  !!'Hello World!'.match(/o/);
+})
+// add listeners
+.on('cycle', function(event, bench) {
+  console.log(String(bench));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+})
+// run async
+.run({ 'async': true });
