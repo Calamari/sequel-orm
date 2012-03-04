fc05356eb225abab718ee997861fef95c9e17e48
Georg Tavonius <georg.tavonius@xing.com>
2012-03-04 12:47:14 +0100

    benchmark testing

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,16 +1,39 @@
 var Benchmark = require("Benchmark");
 
 
-var test = new Benchmark.Suite();
+var test = new Benchmark.Suite('Object key traversal', {
+  setup: function() {
+  }
+});
 
-test.add('RegExp#test', function() {
-  /o/.test('Hello World!');
+var obj = {
+  bla: 'blubb',
+  foo: 42,
+  thing: new Date()
+};
+
+test.add('for var in', function() {
+  for (var key in obj) {
+    keys[key];
+  }
 })
-.add('String#indexOf', function() {
-  'Hello World!'.indexOf('o') > -1;
+.add('for each Object.keys', function() {
+  var keys = Object.keys(obj);
+  for (var i=0,l=keys.length; i<l; ++i) {
+    keys[i];
+  }
 })
-.add('String#match', function() {
-  !!'Hello World!'.match(/o/);
+.add('for each Object.keys reverse', function() {
+  var keys = Object.keys(obj);
+  for (var i=keys.length; i--; ) {
+    keys[i];
+  }
+})
+.add('forEach Object.keys reverse', function() {
+  var keys = Object.keys(obj);
+  keys.forEach(function(key) {
+    key;
+  });
 })
 // add listeners
 .on('cycle', function(event, bench) {
